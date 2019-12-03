a04006a9ca3b45a872e50793f48b70078534b2bb
jaeyun95 <wodbs9522@gmail.com>
2019-12-04 02:38:41 +0900

    update

--- exercise/exercise01.pl
+++ exercise/exercise01.pl
@@ -4,11 +4,11 @@ student(YoungSuk). %YoungSuk is student
 student(JaeLong). %JaeLong is student
 
 %example02
-student(Jaeyun):- happy(Jaeyun). %If JaeYun is happy, JaeYun will student, 
+student(Jaeyun):- happy(Jaeyun). %If JaeYun is happy, JaeYun will student.
 student(YoungSuk):- studyHard(YoungSuk). %If YoungSuk is studey hard, YoungSuk is student.
 student(JaeLong):- unhappy(JaeLong).  %If JaeLong is unhappy, JaeLong is student.
 
 %example02
-loves(JaeYun,YoungSuk)
-loves(YoungSuk,JaeYun)
-loves(JaeYun,JaeLong)
+loves(JaeYun,YoungSuk) %JaeYun loves YoungSuk.
+loves(YoungSuk,JaeYun) %YoungSuk loves JaeYun.
+loves(JaeYun,JaeLong) %JaeYun loves JaeLong.
